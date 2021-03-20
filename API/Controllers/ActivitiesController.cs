597e8fdf491a9cf178357cc7e704c5209a591514
chrifox <confoxt@gmail.com>
2021-03-20 10:51:58 +0000

    Section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : APIController
+    {
+        private readonly DataContext _ctx;
+        public ActivitiesController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _ctx.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _ctx.Activities.FindAsync(id);
+        }
+    }
+}
